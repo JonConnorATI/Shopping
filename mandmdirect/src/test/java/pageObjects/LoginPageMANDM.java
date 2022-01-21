9364c2c4bae3a147e30e225f9608a5c31d977ec3
Jon Connor <jonconnor@live.ie>
2022-01-21 20:00:04 +0000

    Got it working - finally.

--- mandmdirect/src/test/java/pageObjects/LoginPageMANDM.java
+++ mandmdirect/src/test/java/pageObjects/LoginPageMANDM.java
@@ -1,42 +1,42 @@
 package pageObjects;
 
 import static org.junit.Assert.assertEquals;
-
 import org.openqa.selenium.By;
-
 import baseMethods.BaseMethodsMANDM;
 
 public class LoginPageMANDM extends BaseMethodsMANDM {
 
+	// Enter username or email
 	public static void enterUsername(String username) {
 		enterText(By.id("EmailAddress"), username);
-		
+
 	}
 
+	// Enter password
 	public static void enterPassword(String password) {
-		enterText(By.id("Password"),password);
-		
+		enterText(By.id("Password"), password);
+
 	}
 
+	// Press the login button
 	public static void pressLoginButton() {
 		Click(By.cssSelector(".disableableContinueButton"));
-		
+
 	}
 
+	// Assert the error message is displayed
 	public static void checkLoginErrorMessage() {
-		assertEquals(driver.findElement(By.xpath("//*[@class='validation-summary-errors']//li")).getText(), ("Oops! You've entered an incorrect email address or password. Please try again."));
-		
+		assertEquals(driver.findElement(By.xpath("//*[@class='validation-summary-errors']//li")).getText(),
+				("Oops! You've entered an incorrect email address or password. Please try again."));
+
 	}
 
-	public static MyAccountPageMANDM checkTitle (String title) {
+	// Assert the page title matches the expected page title
+	public static MyAccountPageMANDM checkTitle(String title) {
 		assertEquals(driver.getTitle(), (title));
-		
-		return new MyAccountPageMANDM();
-		
-		}
-		
-	
 
-	
+		return new MyAccountPageMANDM();
+
+	}
 
 }
--- mandmdirect/src/test/java/stepDefinitions/StepsLoginMANDM.java
+++ mandmdirect/src/test/java/stepDefinitions/StepsLoginMANDM.java
@@ -7,10 +7,11 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.*;
 import pageObjects.HomePageMANDM;
+
 import pageObjects.LoginPageMANDM;
 
 public class StepsLoginMANDM extends BaseMethodsMANDM {
-	
+
 	@Before()
 	public void setup() throws IOException {
 
@@ -22,43 +23,43 @@ public class StepsLoginMANDM extends BaseMethodsMANDM {
 
 		closeDriver();
 	}
-	
+
 	@Given("I open the website and go to sign in")
 	public void i_open_the_website_and_go_to_sign_in() throws InterruptedException {
 		navigateToHomePage();
 		HomePageMANDM.acceptCookies();
 		HomePageMANDM.selectLoginLink();
 	}
-	
+
 	@When("^The ([^\"]*) is entered into the username field$")
 	public void the_is_entered_into_the_username_field(String username) {
-		
+
 		LoginPageMANDM.enterUsername(username);
 	}
-	
+
 	@When("^([^\"]*) is entered into the password field$")
 	public void is_entered_into_the_password_field(String password) throws Throwable {
 
 		LoginPageMANDM.enterPassword(password);
 	}
-	
+
 	@When("I press Login")
 	public void i_press_login() {
-		
+
 		LoginPageMANDM.pressLoginButton();
 	}
-	
-	@Then("^There should be ([^\"]*) message displayed$")
-	public void there_should_be_message_displayed(String errorMessage) {
-		if (errorMessage == "An Error") {
 
-			LoginPageMANDM.checkLoginErrorMessage();
+	@Then("There should be An Error message displayed")
+	public void there_should_be_an_error_message_displayed() {
 
-		} else {
-			System.out.println("In the else section");
-			LoginPageMANDM.checkTitle("MandM Direct Welcome");
+		LoginPageMANDM.checkLoginErrorMessage();
 
-		}
+	}
+
+	@Then("There should be No Error message displayed")
+	public void there_should_be_no_error_message_displayed() {
+		System.out.println("In the else section");
+		LoginPageMANDM.checkTitle("MandM Direct Welcome");
 
 	}
 
