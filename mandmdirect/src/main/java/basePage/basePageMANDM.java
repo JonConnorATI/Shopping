7204d7e725781ae19be9db6abb36848c890eca5c
Jon Connor <jonconnor@live.ie>
2022-03-21 20:50:09 +0000

    general updates and method to use different webdrivers

--- mandmdirect/src/main/java/basePage/basePageMANDM.java
+++ mandmdirect/src/main/java/basePage/basePageMANDM.java
@@ -4,36 +4,83 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class basePageMANDM {
 	
-	//Declare the webdriver 
-	public static WebDriver driver;
+	// Write here what browser you want to test with
+		private static String browser = /* write browser here ---> */ "edge";
 
-	//Set the webdriver that all scenarios use
-	public static void setDriver() {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\JonCo\\git\\Shopping\\mandmdirect\\Drivers\\chromedriver.exe");
-				driver = new ChromeDriver();
+		// Declare the web driver
+		public static WebDriver driver;
 
-	}
+		// Set the web driver that all scenarios use
+		public static WebDriver setDriver() {
 
-	//open the url
-	public static void navigateToHomePage() {
-		driver.manage().window().maximize();
-		//driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
-		String homePage = "https://www.mandmdirect.ie/";
-		driver.get(homePage);
+			try {
 
-	}
+				String browserName = browser;
 
-	//close the webdriver and clear cookies
-	public static void closeDriver() {
-		driver.manage().deleteAllCookies();
-		driver.close();
-		driver.quit();
+				switch (browserName) {
 
-	}
+				// fire fox setup
+				case "firefox":
+					if (null == driver) {
+						System.setProperty("webdriver.gecko.driver",
+								"C:\\Users\\JonCo\\git\\Shopping\\mandmdirect\\Drivers\\geckodriver.exe");
+						driver = new FirefoxDriver();
+					}
+					break;
 
+				// chrome setup
+				case "chrome":
+					if (null == driver) {
+						System.setProperty("webdriver.chrome.driver",
+								"C:\\Users\\JonCo\\git\\Shopping\\mandmdirect\\Drivers\\chromedriver.exe");
+						driver = new ChromeDriver();
+					}
+					break;
+
+				// Edge setup
+				case "edge":
+					if (null == driver) {
+
+						System.setProperty("webdriver.edge.driver",
+								"C:\\Users\\JonCo\\git\\Shopping\\mandmdirect\\Drivers\\msedgedriver.exe");
+						driver = new EdgeDriver();
+					}
+					break;
+				}
+			} catch (Exception e) {
+				System.out.println("Unable to load browser! - Exception: " + e.getMessage());
+			} finally {
+				driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+			}
+			return driver;
+		}
+
+		// open the url
+		public static void navigateToHomePage() {
+			driver.manage().window().maximize();
+			// driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+			String homePage = "https://www.mandmdirect.ie/";
+			driver.get(homePage);
+
+		}
+
+		// close the web driver and clear cookies
+		public static void closeDriver() {
+
+			try {
+				if (driver != null) {
+					driver.manage().deleteAllCookies();
+					driver.quit();
+					driver = null;
+				}
+			} catch (Exception e) {
+				System.out.println("Method Failed: screenshotOnFailure, Exception: " + e.getMessage());
+			}
+		}
 }
